2bb6d9e4ae707f32880f34daa7bca5e0e9bd54d3
Kira M. OGrady <ko805626@WS-ACADBUS.EDU>
2022-02-11 08:42:39 -0800

    2/11/2022 pt2

--- Comp Sci 1/Lab 29 Space Curves Phase 1/Sketch.js
+++ Comp Sci 1/Lab 29 Space Curves Phase 1/Sketch.js
@@ -1,14 +1,14 @@
-let sun, planet, moon;
+let sun, planet, moon;//declare global variables
 function setup() {
   createCanvas(400, 400);
-  sun = new Sun(width/2, height/2)
+  sun = new Sun(width/2, height/2)//creating classes
   planet = new Planet();
   moon = new Moon();
 }
 
 function draw() {
-  background(220);
-  sun.run();
+  background(20);
+  sun.run();//run objects
   planet.run();
   moon.run();
 }
--- Comp Sci 1/Lab 29 Space Curves Phase 1/moon.js
+++ Comp Sci 1/Lab 29 Space Curves Phase 1/moon.js
@@ -1,8 +1,8 @@
 class Moon{
   constructor(){
-    this.loc = createVector(0, 0)
+    this.loc = createVector(0, 0)//since location is determined by planet it can be 0
     this.vel = createVector(0, 0);
-    this.mor = 50;
+    this.mor = 50;//moon orbital radius
     this.angle = 0;
   }
 
@@ -12,15 +12,15 @@ class Moon{
   }
 
   update(){
-    this.loc.add(this.vel);
-    this.angle = this.angle + 0.05
+    this.loc.add(this.vel);//add velocity to location
+    this.angle = this.angle + 0.05//change angle
 
-    moon.loc.x = planet.loc.x + cos(this.angle)*this.mor
+    moon.loc.x = planet.loc.x + cos(this.angle)*this.mor//orbiting code
     moon.loc.y = planet.loc.y + sin(this.angle)*this.mor
   }
 
   render(){
     fill(0, 200, 255)
-    ellipse(this.loc.x, this.loc.y, 10);
+    ellipse(this.loc.x, this.loc.y, 10);//draw ellipse
   }
 }//+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
