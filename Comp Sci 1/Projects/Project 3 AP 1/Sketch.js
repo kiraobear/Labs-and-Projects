45838679179bb40d8d7a15b9bc88b995bc5bbab6
Kira O'Grady <kograobear@gmail.com>
2022-02-18 13:34:19 -0800

    2/18/2022 pt idk

--- Comp Sci 1/Projects/Project 3 AP 1/Sketch.js
+++ Comp Sci 1/Projects/Project 3 AP 1/Sketch.js
@@ -1,21 +1,25 @@
 //Kira O'Grady
 //Art Project 1 215
 let movers = [];
-let mainMover;
+let mainMover,distToMainMover ;
 let segments = [];
 let v;
 let pause = false;
+let slider,val;
 function setup() {
-  // createCanvas(400, 400);
+  //createCanvas(400, 400);
   createCanvas(windowWidth, windowHeight);
-  // background(10, 10, 10);
-
-  loadMovers(175);
+  background(10, 10, 10);
+  colorMode(HSB);
+  slider = createSlider(0, 360, 60, 40);
+  slider.position(10, 10);
+  slider.style('width', '80px');
+  loadMovers(25);
 }
 
 function draw() {
   if (!pause){
-    background(10, 10, 10, 10);
+  //  background(10, 10, 10, 10);
     mainMover.run();
     for(let i = 0; i < movers.length; i++){
       movers[i].run();
@@ -24,8 +28,8 @@ function draw() {
     for (let i = 0; i < segments.length; i++){
       segments[i].run();
     }
-
   }
+  let val = slider.value();
 }
 
 function keyPressed(){
--- Comp Sci 1/Projects/Project 3 AP 1/movers.js
+++ Comp Sci 1/Projects/Project 3 AP 1/movers.js
@@ -4,7 +4,7 @@ class Mover{
     this.vel = createVector(random(-5,5));
     this.acc = createVector(0,0);
     this.diam = 15
-    this.clr = color(255);
+    this.clr = (0, 0, 255);
   }
   run(){
     this.update();
@@ -13,46 +13,47 @@ class Mover{
     this.checkDistance();
   }
   update(){
-let distToMainMover = this.loc.dist(mainMover.loc);
-//++++++++++++++++++++++++++
-this.acc = p5.Vector.sub(mainMover.loc, this.loc);
-this.acc.normalize();
-this.acc.mult(0.03);
-//+++++++++++++++++++++++++
-if(distToMainMover < 145){
-this.acc = p5.Vector.sub(this.loc, mainMover.loc);
-this.acc.normalize();
-this.acc.mult(0.09);
-  }
-  this.vel.add(this.acc);
-  if(this === mainMover){
-    this.vel.limit(3);
-  } else{
-    this.vel.limit(3);
-  }
-  this.loc.add(this.vel);
-}
-  render(){
-    noStroke();
-
-    if(this === mainMover){
-      this.clr = color(10,28,142);
-    } else{
-      this.clir = color(255);
+    distToMainMover = this.loc.dist(mainMover.loc);
+  //++++++++++++++++++++++++++
+  this.acc = p5.Vector.sub(mainMover.loc, this.loc);
+  this.acc.normalize();
+  this.acc.mult(0.03);
+  //+++++++++++++++++++++++++
+  if(distToMainMover < 145){
+  this.acc = p5.Vector.sub(this.loc, mainMover.loc);
+  this.acc.normalize();
+  this.acc.mult(0.09);
     }
-    fill(this.clr);
-    ellipse(this.loc.x, this.loc.y, this.diam);
+    this.vel.add(this.acc);
+    if(this === mainMover){
+      this.vel.limit(3);
+    } else{
+      this.vel.limit(3);
+    }
+    this.loc.add(this.vel);
+  /*let v = random(1);
+    if(v ===0){
+      this.clr = (255, 255, 0);
+    } else {
+      this.clr = (0,0,255);
+    }*/
+  }
+    render(){
+      noStroke();
+      /*if(this === mainMover){
+        this.clr = color(10,28,142);
+      } else{
+        this.clr = color(255);
+      }*/
+      fill(this.clr);
+      //ellipse(this.loc.x, this.loc.y, this.diam);
 
-
-
-    for (let i = 0; i < movers.length; i++){
-      if (this === movers[i]){
-        // segments.push(new Segment(i));
-        //segments[i] = new Segment(i);
-
+      for (let i = 0; i < movers.length; i++){
+        if (this === movers[i]){
+          // segments.push(new Segment(i));
+          //segments[i] = new Segment(i);
       }
     }
-
   }
   checkEdges(){
     if(this.loc.x > width || this.loc.x < 0){
@@ -65,11 +66,19 @@ this.acc.mult(0.09);
 
   checkDistance(){
     for(let i = 0; i < movers.length; i++){
+    if(distToMainMover < 100){
+      this.clr = (255, 255, 0);
+    } else {
+      this.clr = (0, 0, 255);
+    }
+  }
+    for(let i = 0; i < movers.length; i++){
       if(movers[i] !== this){
         let dis = this.loc.dist(movers[i].loc);
         if(dis < 50){
             stroke(this.clr);
-            strokeWeight(0.09)
+            let s = random(0.03, 0.1);
+            strokeWeight(s)
             line(this.loc.x, this.loc.y, movers[i].loc.x, movers[i].loc.y);
         }
       }
