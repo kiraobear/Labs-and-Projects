bcb34568c7819a1e4292ad8455d9993116f83d10
Kira M. OGrady <ko805626@WS-ACADBUS.EDU>
2022-03-07 08:47:08 -0800

    3/7/2022

--- Comp Sci 1/Projects/Project 3 AP 3/Sketch.js
+++ Comp Sci 1/Projects/Project 3 AP 3/Sketch.js
@@ -1,7 +1,23 @@
+let roots;
 function setup() {
   createCanvas(400, 400);
+  roots = [];
+
+  roots.push(new Root(brush.loc.x, brush.loc.y, 25));
 }
 
 function draw() {
   background(220);
+  for(let i = 0; i < roots.length; i++){
+      roots[i].run();
+    }
+  checkRoots();
+}
+
+function checkRoots(){
+  for(let i = roots.length - 1; i >= 0; i--) {
+    if(roots[i].isDead) {
+      roots.splice(i, 1);
+    }
+  }
 }
---
+++ Comp Sci 1/Projects/Project 3 AP 3/root.js
@@ -0,0 +1,35 @@
+class Root{
+constructor(x, y, diam) {
+    this.loc = createVector(x, y);
+    this.vel = createVector(random (-3,3), random(-3, 3));
+    this.acc = createVector(0,0);
+    this.diam = diam;
+    this.lifespan = 900;
+    this.isDead = false;
+    this.value = ceil(random(100));
+  }
+  run(){
+    this.render();
+    this.update();
+  }
+  update() {
+    this.lifespan --;
+    if (this.lifespan < 0 || this.diam < 0) {
+      this.isDead = true;
+    }
+    if( this.diam === 20){
+      roots.push(new Root(this.loc.x, this.loc.y, this.diam))
+    }
+    this.diam = this.diam - 0.1;
+    this.acc = createVector(random (-0.83,0.83), random(-0.83, 0.83));
+    this.vel.add(this.acc);
+    this.vel.limit(3);
+    this.loc.add(this.vel);
+  }
+  render(){
+    strokeWeight(0.3);
+    stroke(255)
+    noFill();
+    ellipse(this.loc.x, this.loc.y, this.diam, this.diam);
+    }
+  }
